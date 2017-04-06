e8131ed4a26f9dd859b6e863b0299cfcb0d4d102
Dakota Bryant <bryant.dakota@gmail.com>
2017-04-06 14:36:39 -0500

    added for loop for comparison array

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 // your code here!
 
 $(document).ready(function() {
-	$('.js-form').submit(function(event){
-		event.preventDefault();
-	let userInput = $('#user-text').val();
-	let totalWords = userInput.split(" ").length;
-	
-	});
+    $('.js-form').submit(function(event) {
+        event.preventDefault();
+        let userInput = $('#user-text').val();
+        let onlyWords = userInput.toLowerCase().replace(/[^\w\s]/g, "");
+        let wordsArray = onlyWords.split(' ')
+        let totalWords = wordsArray.length;
 
+        function uniqueWords(comparedArray) {
+            let comparisonArray = []
+            for (i = 0; i < comparedArray.length; i++) {
+                if (!comparisonArray.includes(comparedArray[i])) {
+                    comparisonArray.push(comparedArray[i]);
+                }
+            }
+            console.log(comparisonArray);
+        }
+				uniqueWords(wordsArray);
 
+    });
 });
-
-
-	
-
-
-
