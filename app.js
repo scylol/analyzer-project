5942fb2b3e3d8363ee1da9018b733673e85b0e6a
Scylol <djd3dobbin@gmail.com>
2017-04-06 15:00:41 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// your code here!
+
+$(document).ready(function() {
+	$('.js-form').submit(function(event){
+		event.preventDefault();
+	let userInput = $('#user-text').val();
+	let totalWords = userInput.split(" ").length;
+	
+	});
+
+
+});
+
+
+	
+
+
+
